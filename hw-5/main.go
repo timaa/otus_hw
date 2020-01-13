d8ec2ef6cadb4e30bf95e9a1d2bf58af4a8bb460
Тимур Аипов <t.aipov@fin.media>
2020-01-13 18:05:59 +0300

    hw-5 raw version

--- hw-5/main.go
+++ hw-5/main.go
@@ -1,1 +1,37 @@
-package hw_5
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/timaa/otus_hw/hw-5/worker_pools"
+	"time"
+)
+
+func main() {
+	funcs:= []func() error {f1, f2, f3, f4}
+	worker_pools.Run(funcs, 4, 4)
+	time.Sleep(1*time.Second)
+}
+
+func f1() error{
+	fmt.Println("f1")
+	return errors.New("f1 err")
+}
+
+func f2() error{
+	fmt.Println("f2")
+	return errors.New("f2 err")
+
+}
+
+func f3() error{
+	fmt.Println("f3")
+	return errors.New("f3 err")
+
+}
+
+func f4() error{
+	fmt.Println("f4")
+	return errors.New("f4 err")
+
+}
---
+++ hw-5/worker_pools/workers.go
@@ -0,0 +1,57 @@
+package worker_pools
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type threadSafeCounter struct {
+	mu      sync.Mutex
+	counter int
+	maxErrorCount int
+}
+
+func (t *threadSafeCounter) Inc() {
+	t.mu.Lock()
+	t.counter++
+	t.mu.Unlock()
+}
+
+func (t threadSafeCounter) Value() int {
+	return t.counter
+}
+
+func Run(tasks []func() error, N int, M int) error {
+	funcs := make(chan func() error, 1)
+	counter := &threadSafeCounter{maxErrorCount: M}
+
+	for i := 0; i < N; i++ {
+		go worker(i, funcs, counter)
+	}
+
+	for j := 0; j < M; j++ {
+		funcs <- tasks[j]
+	}
+
+	time.Sleep(1 * time.Second)
+
+	return nil
+}
+
+func worker(id int, funcs chan func() error, counter *threadSafeCounter) {
+
+
+	fmt.Printf("counter=%d \n", counter.Value())
+	for f := range funcs {
+		fmt.Printf("Worker with id %d \n", id)
+		err := f()
+		if err != nil {
+			counter.Inc()
+			if counter.counter >= counter.maxErrorCount {
+				close(funcs)
+			}
+		}
+	}
+	fmt.Printf("closed %d goroutine \n", id)
+}
