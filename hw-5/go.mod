6899e8df21ef00f3f9c9c632ce720249b2c0589f
Тимур Аипов <timuraipov@MacBook-Pro-Timur-2.local>
2020-02-01 18:04:02 +0300

    hw-5

---
+++ hw-5/go.mod
@@ -0,0 +1,3 @@
+module github.com/timaa/otus_hw/hw-5
+
+go 1.13
--- hw-5/main.go
+++ hw-5/main.go
@@ -3,26 +3,22 @@ package main
 import (
 	"errors"
 	"fmt"
-	"github.com/timaa/otus_hw/hw-5/worker_pools"
-	"time"
+	"github.com/timaa/otus_hw/hw-5/workerpools"
 )
 
 func main() {
 	funcs:= []func() error {f1, f2, f3, f4}
-	worker_pools.Run(funcs, 4, 4)
-	time.Sleep(1*time.Second)
+	workerpools.Run(funcs, 4, 4)
 }
 
 func f1() error{
 	fmt.Println("f1")
 	return nil
-	//return errors.New("f1 err")
 }
 
 func f2() error{
 	fmt.Println("f2")
 	return nil
-	//return errors.New("f2 err")
 }
 
 func f3() error{
